d4eb0bb41901ff0ab53bd80b0e204d853e2cc331
Jim <a9372275@9372275s-MacBook-Pro.local>
2019-04-30 11:14:19 +0100

    server built

---
+++ client/src/app.js
@@ -0,0 +1,17 @@
+// const CharacterView = require('./views/character_view.js');
+// const Characters = require('./models/characters.js');
+// const CharacterFormView = require('./views/character_form_view.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Content loaded.');
+  // const characterContainer = document.querySelector('#character-container');
+  // const characterView = new CharacterView(characterContainer);
+  // characterView.bindEvents();
+  //
+  // const formElement = document.querySelector("#character-form");
+  // const characterFormView = new CharacterFormView(formElement);
+  // characterFormView.bindEvents();
+  //
+  // const characters = new Characters();
+  // characters.getData();
+});
