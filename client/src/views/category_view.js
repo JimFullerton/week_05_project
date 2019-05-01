c192f6d675a87c33a6c9f2dfca2645ce053fe2ea
Jim <a9372275@9372275s-MacBook-Pro.local>
2019-05-01 13:12:42 +0100

    cat listener works

--- client/src/views/category_view.js
+++ client/src/views/category_view.js
@@ -11,11 +11,17 @@ class CategoryView{
       const categories = evt.detail;
       console.log('In cat view, cats retrieved: ', categories);
       this.render(categories);
+      this.catSelection();
     });
   }
 
   render(categories) {
     this.clearCategories();
+
+    const zeroOption = document.createElement('option');
+    zeroOption.textContent = "";
+    zeroOption.value = 0;
+    this.container.appendChild(zeroOption);
 
     categories.forEach((cat) => {
       const newOption = document.createElement('option');
@@ -27,6 +33,27 @@ class CategoryView{
 
   clearCategories() {
     this.container.innerHTML = '';
+  }
+
+  catSelection() {
+    console.log("in cat select");
+    const catFilter = document.querySelector('select#category');
+    catFilter.addEventListener('change', (evt) => {
+      evt.preventDefault();
+      console.log("in filter evt", evt.target.value);
+      // const newAlbum = {};
+      // newAlbum.title = evt.target['title'].value;
+      // newAlbum.artist = evt.target['artist'].value;
+      // newAlbum.year = evt.target['year'].value;
+      // newAlbum.genre = evt.target['genre'].value;
+      // newAlbum.label = evt.target['label'].value;
+      //
+      // const albums = new Albums();
+      // albums.postAlbum(newAlbum);
+
+      // this.element.reset();
+    });
+
   }
 
 }
